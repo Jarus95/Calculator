dfc8682b8413981a1314f1e72ca386a7d6fa7508
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-17 11:23:18 +0500

    Add program file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace  Calculator
+{
+    class Calculator
+    {
+       static void Main()
+       {
+          Console.WriteLine("Enter Values");
+          Console.Write("Enter first number: ");
+          string firstValue= Console.ReadLine();
+
+           Console.Write("Enter second number: ");
+           string secondValue = Console.ReadLine();
+           Console.WriteLine("Converting values...");
+           decimal firsNumber = decimal.Parse(firstValue);
+           decimal secondNumber = decimal.Parse(secondValue);
+           Console.WriteLine($"{firsNumber} + {secondNumber} = {firsNumber + secondNumber}");
+           Console.WriteLine($"{firsNumber} - {secondNumber} = {firsNumber - secondNumber}");
+           Console.WriteLine($"{firsNumber} * {secondNumber} = {firsNumber * secondNumber}");
+           Console.WriteLine($"{firsNumber} / {secondNumber} = {firsNumber / secondNumber}");
+
+
+
+       }
+    }
+}
