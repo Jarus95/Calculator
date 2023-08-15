f443c94bf4015f05e1574d8d8f384f20189814a2
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-15 10:33:41 +0500

    Add Advanced Calculator

--- Classes/Menu.cs
+++ Classes/Menu.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace MyCalculator.Classes
 {
     public class Menu
     {
         public void ShowMenu()
         {
-             Console.WriteLine("1. Show multiplication table");
-             Console.WriteLine("2. Mathematical operations");
+            Console.WriteLine("1. Show multiplication table");
+            Console.WriteLine("2. Mathematical operations");
         }
     }
 }
--- Classes/MultiplicationTable.cs
+++ Classes/MultiplicationTable.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace MyCalculator.Classes
 {
     public class MultiplicationTable
     {
         public void ShowMultiplicationTable()
         {
-             for(int outerIteration = 2; outerIteration <= 9; outerIteration++)
-             {
-                 Console.WriteLine(new string('-', 50));
-                 for(int iteration = 1; iteration <= 9; iteration++)
-                 {
+            for (int outerIteration = 2; outerIteration <= 9; outerIteration++)
+            {
+                Console.WriteLine(new string('-', 50));
+                for (int iteration = 1; iteration <= 9; iteration++)
+                {
                     Console.WriteLine($"{outerIteration} * {iteration} = {outerIteration * iteration}");
-                 }
-             }
+                }
+            }
         }
     }
 }
